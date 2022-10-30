9b59276d73b1e1cdf6ad235ae1f7cd800d3ec195
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-10-29 21:43:15 -0700

    single sprite change

--- Lua/Abilities.lua
+++ Lua/Abilities.lua
@@ -1,24 +1,35 @@
-freeslot("S_MISSLE_JUMP")
+freeslot("S_MISSLE_JUMP", "SPR2_MJMP");
 
---Ability States
+-- Ability States
 states[S_MISSLE_JUMP] = {
 	sprite = SPR_PLAY,
-	tics = 20,
-	nextstate = S_PLAY_FALL
+	frame = SPR2_MJMP,
+	tics = 100,
+	nextstate = S_PLAY_FALL;
 }
+
+local function stateFunc(p1)
+	print(p1);
+end
 
 
 local abltyEnbl = {
 	MS = true;
 }
 
+local function abilityReset(player)
+	-- Reset Missle Swarp
+	if(P_IsObjectOnGround(player.mo)) then
+		abltyEnbl.MS = true;
+	end
+end
 
 -- Tries to perform Slash Attack, returns true if succedes, false if not
 local function slash(player)
 
 end
 
-
+-- Double Jumps and shoots missiles in all directions
 local function missleSwarm(player) 
 	player.mo.state = S_MISSLE_JUMP;
 	local enemyMobj = P_LookForEnemies(player);
@@ -36,15 +47,8 @@ end
 
 
 
-local function abilityReset(player)
-	-- Reset Missle Swarp
-	if(P_IsObjectOnGround(player.mo)) then
-		abltyEnbl.MS = true;
-	end
-end
-
 addHook("AbilitySpecial", function (player)
-	-- If Jump is pressed in airPF_JUMPDOWN
+	-- If Jump is pressed in air
 	if(abltyEnbl.MS) then
 		missleSwarm(player);
 	end
@@ -56,5 +60,10 @@ addHook("PlayerThink", function(player)
 		print("Wrong skin: " .. player.mo.skin);
 		return
 	end
+	if(player.mo.state == S_MISSLE_JUMP) then
+		print("S_MISSLE_JUMP: " .. S_MISSLE_JUMP);
+-- 		print(states[S_MISSLE_JUMP].tics);
+	end
+	print(player.mo.tics);
 	abilityReset(player);
 end);
