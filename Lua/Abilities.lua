b0e2723e5461f1fa77d0d22c8c8e90aeccfbd977
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-11-13 18:44:26 -0800

    Basic Slide ability added

--- Lua/Abilities.lua
+++ Lua/Abilities.lua
@@ -31,29 +31,38 @@ mobjinfo[MT_S5_MISSILE] = {
 	flags = MF_MISSILE|MF_NOGRAVITY	
 }
 
-local missMaxDist = 300*FRACUNIT;
+local missMaxDist = 300*FRACUNIT
+local shouldSld = false
+local shouldMs = false
+local slideSpeedCap = 5*FRACUNIT
 
 -- Performs Slide ability on spin if on the ground
 local function slide(player)
 	if(not player.mo.name == "subject5"
-	or not player.shouldSld
+-- 	or shouldSld == false
 	or not P_IsObjectOnGround(player.mo)) then 
 		return 
 	end
-	
-	if(not player.mo.state == S_SLIDE) then
-		print(player.mo.state)
-		P_InstaThrust(player.mo, player.mo.angle, 20*FRACUNIT)
+	-- Thrusting when sliding once
+	if(not (player.mo.state == S_SLIDE)) then
+		local speed = player.mo.scale
+-- 		print(speed)
+		P_InstaThrust(player.mo, player.drawangle, FixedMul(speed, 25*FRACUNIT))
 	end
+	-- Other stuff
+	
 	player.mo.state = S_SLIDE
-
+-- 	player.speed = $1 - FixedDiv($1, 2)
+	player.mo.friction = FRACUNIT-10000
 	player.height = P_GetPlayerSpinHeight(player)
+	player.charflags = $1|SF_CANBUSTWALLS
+	player.pflags = $1|PF_JUMPED
 end
 
 local function tryResetMissiles(player)
 	-- Reset Missile Swarp 
 	if(P_IsObjectOnGround(player.mo)) then
-		player.shouldMS = true
+		shouldMs = true
 	end
 end
 
@@ -101,8 +110,8 @@ end;
 local function removeLockOn(mobj)
 	if(not !mobj.lock) then
 		print("lock was nil?");
-		return;
-	end;
+		return
+	end
 	local mobj1 = mobj.lock;
 	mobj.lock = nil;
 	if(not !mobj.lock) then
@@ -115,8 +124,9 @@ end
 -- Double Jumps and shoots missiles in all directions
 local function missileSwarm(player) 
 	if(not player.mo.name == "subject5" or
-	not player.shouldMS) then return end
-	player.mo.state = S_MISSILE_JUMP;
+	shouldMs == false) then return
+	end
+	player.mo.state = S_MISSILE_JUMP
 	for i = 0, 3 do
 		 P_SPMAngle(player.mo, MT_S5_MISSILE, player.mo.angle + FixedAngle(i*90*FRACUNIT))
 	end
@@ -124,12 +134,8 @@ local function missileSwarm(player)
 	-- Vertical Boost for player
 	P_SetObjectMomZ(player.mo, FixedMul(10*FRACUNIT, player.mo.scale));
 
-	player.shouldMS = false
+	shouldMs = false
 end
-
-addHook("AbilitySpecial", missileSwarm);
-
-addHook("SpinSpecial", slide)
 
 addHook("PlayerThink", function(player)
 	-- Stop function if the player is not Subjct5
@@ -137,24 +143,19 @@ addHook("PlayerThink", function(player)
 	
 	tryResetMissiles(player)
 	
-	if(player.shouldMS == nil) then
-		player.shouldMS = true
-	end
-	if(player.shouldSld == nil) then
-		
-		player.shouldSld = true
-	end
-	
+	-- Slide Ability button is released
 	if(not (player.cmd.buttons & BT_SPIN) 
 	and player.mo.state == S_SLIDE) then
-		print("Spin released");
--- 		player.shouldSld = false
+		player.height = skins[player.mo.skin].height -- returns the default height value
 		player.mo.state = S_PLAY_STND
+		-- Undo enemy damage and wallbreak 
+		player.charflags = $ & ~SF_CANBUSTWALLS
+		player.pflags = $ & ~PF_JUMPED
 	end
 	
 	-- Stop sliding if speed is too small
-	player.shouldSld = not (player.mo.state == S_SLIDE
-						and player.speed < FixedMul(15*FRACUNIT, player.mo.scale))
+-- 	shouldSld = not (player.mo.state == S_SLIDE
+-- 						and player.speed < FixedMul(15*FRACUNIT, player.mo.scale))
 end)
 
 addHook("MobjCollide", 
@@ -162,12 +163,30 @@ addHook("MobjCollide",
 			removeLockOn(missile)
 		end,
 		MT_S5_MISSILE);
+
 addHook("MobjDeath", 
 		function(missile, p1, p2, p3) 
 			removeLockOn(missile)
 		end,
 		MT_S5_MISSILE)
+			
+-- When player colliding with the enemy
+addHook("MobjCollide", 
+		function(thing, thing2)
+			if(thing.state == S_SLIDE
+			and thing2.flags & MF_ENEMY) then
+				P_InstaThrust(thing2, 
+							   thing.player.drawangle, 
+							   FixedMul(20, thing.player.speed))
+			end
+		end, 
+		MT_PLAYER)
 		
+-- Use ability Swarm on Jump key pressed in the air
+addHook("AbilitySpecial", missileSwarm);
+-- Slide on spin key hold
+addHook("SpinSpecial", slide)
+
 -- Tries to replace a wandering missile with lock-on missile
 addHook("MobjThinker", tryMissileLock, MT_S5_MISSILE);
 -- Ignores specific missile targets
